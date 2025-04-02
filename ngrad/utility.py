cac12393db6a136a3d4438c7c648ac503bc27868
Ivan Bioli <ivan.bioli@gmail.com>
2025-04-02 10:30:59 +0200

    line-search with tree_map

--- ngrad/utility.py
+++ ngrad/utility.py
@@ -5,6 +5,8 @@ Contains simple helper functions.
 
 import jax.numpy as jnp
 from jax import grad, hessian, vmap, jit
+import jax.numpy as jnp
+from jax import jit, vmap, tree_map
 
 def del_i(g, argnum=0):
     """
@@ -33,10 +35,8 @@ def laplace(func):
     return lambda x: jnp.sum(jnp.diag(hesse(x)))
 
 def grid_line_search_factory(loss, steps):
-    
     def loss_at_step(step, params, tangent_params):
-        updated_params = [(w - step * dw, b - step * db)
-            for (w, b), (dw, db) in zip(params, tangent_params)]
+        updated_params = tree_map(lambda p, dp: p - step * dp, params, tangent_params)
         return loss(updated_params)
         
     v_loss_at_steps = jit(vmap(loss_at_step, (0, None, None)))    
@@ -45,7 +45,10 @@ def grid_line_search_factory(loss, steps):
     def grid_line_search_update(params, tangent_params):
         losses = v_loss_at_steps(steps, params, tangent_params)
         step_size = steps[jnp.argmin(losses)]
-        return [(w - step_size * dw, b - step_size * db)
-                for (w, b), (dw, db) in zip(params, tangent_params)], step_size
-    
+        updated_params = tree_map(
+            lambda p, dp: p - step_size * dp, params, tangent_params
+        )
+
+        return updated_params, step_size
+
     return grid_line_search_update
